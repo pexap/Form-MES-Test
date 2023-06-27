2116b43bd1be4d24ab8a61241a7ffa06ac401716
Luxferre <scoeg12@gmail.com>
2023-06-27 12:06:29 +0700

    Add project files.

---
+++ Form MES Test/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Form_MES_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_MoveStd = new System.Windows.Forms.Button();
+            this.button_AddFactory = new System.Windows.Forms.Button();
+            this.textBox_Log = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_UpdateFactory = new System.Windows.Forms.Button();
+            this.button_DelFactory = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_MoveStd
+            // 
+            this.button_MoveStd.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button_MoveStd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_MoveStd.Location = new System.Drawing.Point(121, 160);
+            this.button_MoveStd.Name = "button_MoveStd";
+            this.button_MoveStd.Size = new System.Drawing.Size(111, 62);
+            this.button_MoveStd.TabIndex = 0;
+            this.button_MoveStd.Text = "Move Std";
+            this.button_MoveStd.UseVisualStyleBackColor = true;
+            this.button_MoveStd.Click += new System.EventHandler(this.btn_MoveStd);
+            // 
+            // button_AddFactory
+            // 
+            this.button_AddFactory.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button_AddFactory.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_AddFactory.Location = new System.Drawing.Point(238, 160);
+            this.button_AddFactory.Name = "button_AddFactory";
+            this.button_AddFactory.Size = new System.Drawing.Size(111, 62);
+            this.button_AddFactory.TabIndex = 1;
+            this.button_AddFactory.Text = "Add Factory";
+            this.button_AddFactory.UseVisualStyleBackColor = true;
+            this.button_AddFactory.Click += new System.EventHandler(this.btn_FactoryAdd);
+            // 
+            // textBox_Log
+            // 
+            this.textBox_Log.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox_Log.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_Log.Location = new System.Drawing.Point(69, 270);
+            this.textBox_Log.Multiline = true;
+            this.textBox_Log.Name = "textBox_Log";
+            this.textBox_Log.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_Log.Size = new System.Drawing.Size(583, 89);
+            this.textBox_Log.TabIndex = 2;
+            this.textBox_Log.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(125, 235);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Log";
+            // 
+            // button_UpdateFactory
+            // 
+            this.button_UpdateFactory.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button_UpdateFactory.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_UpdateFactory.Location = new System.Drawing.Point(355, 160);
+            this.button_UpdateFactory.Name = "button_UpdateFactory";
+            this.button_UpdateFactory.Size = new System.Drawing.Size(111, 62);
+            this.button_UpdateFactory.TabIndex = 4;
+            this.button_UpdateFactory.Text = "Update Factory";
+            this.button_UpdateFactory.UseVisualStyleBackColor = true;
+            this.button_UpdateFactory.Click += new System.EventHandler(this.btn_FactoryUpdate);
+            // 
+            // button_DelFactory
+            // 
+            this.button_DelFactory.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button_DelFactory.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_DelFactory.Location = new System.Drawing.Point(472, 160);
+            this.button_DelFactory.Name = "button_DelFactory";
+            this.button_DelFactory.Size = new System.Drawing.Size(111, 62);
+            this.button_DelFactory.TabIndex = 5;
+            this.button_DelFactory.Text = "Delete Factory";
+            this.button_DelFactory.UseVisualStyleBackColor = true;
+            this.button_DelFactory.Click += new System.EventHandler(this.btn_FactoryDelete);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_DelFactory);
+            this.Controls.Add(this.button_UpdateFactory);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_Log);
+            this.Controls.Add(this.button_AddFactory);
+            this.Controls.Add(this.button_MoveStd);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_MoveStd;
+        private System.Windows.Forms.Button button_AddFactory;
+        private System.Windows.Forms.TextBox textBox_Log;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_UpdateFactory;
+        private System.Windows.Forms.Button button_DelFactory;
+    }
+}
+
---
+++ Form MES Test/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Camstar.WCF.Services;
+using Camstar.WCF.ObjectStack;
+using Camstar.Util;
+using Camstar.Exceptions;
+
+namespace Form_MES_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string id = "Administrator";
+        string pw = "Ocg123!";
+
+        private void btn_MoveStd(object sender, EventArgs e)
+        {
+            try
+            {
+                MoveStdService oCDOService = new MoveStdService(new UserProfile(id, pw));
+                MoveStd oCDO = new MoveStd { Container = new ContainerRef("AAA") };
+                ResultStatus oResultStatus = oCDOService.ExecuteTransaction(oCDO);
+
+                if (oResultStatus.IsSuccess) textBox_Log.AppendText(oResultStatus.Message + System.Environment.NewLine);
+                else throw new Exception(oResultStatus.ExceptionData.Description);
+            }
+            catch (Exception ex)
+            {
+                textBox_Log.AppendText(ex.Message + System.Environment.NewLine);
+            }
+        }
+
+        private void btn_FactoryAdd(object sender, EventArgs e)
+        {
+            try
+            {
+                FactoryMaintService oCDOService = new FactoryMaintService(new UserProfile(id, pw));
+                FactoryMaint oCDO = new FactoryMaint();
+                oCDO.ObjectChanges = new FactoryChanges { Name = "Factory 1", Description = "My First Factory" };
+                oCDOService.BeginTransaction();
+                oCDOService.New(oCDO);
+                oCDOService.ExecuteTransaction();
+
+                ResultStatus oResultstatus = oCDOService.CommitTransaction();
+
+                if (oResultstatus.IsSuccess) textBox_Log.AppendText(oResultstatus.Message + System.Environment.NewLine);
+                else throw new Exception(oResultstatus.ExceptionData.Description);
+            }
+            catch(Exception ex)
+            {
+                textBox_Log.AppendText(ex.Message + System.Environment.NewLine);
+            }
+        }
+
+        private void btn_FactoryUpdate(object sender, EventArgs e)
+        {
+            try
+            {
+                FactoryMaintService oCDOService = new FactoryMaintService(new UserProfile(id, pw));
+                FactoryMaint oCDO = new FactoryMaint();
+                oCDO.ObjectToChange = new NamedObjectRef { Name = "Factory 1" };
+                oCDOService.BeginTransaction();
+                oCDOService.Load(oCDO);
+
+                oCDO = new FactoryMaint();
+
+                oCDO.ObjectChanges = new FactoryChanges();
+                oCDO.ObjectChanges.Description = "Factory 1 Modified";
+                oCDOService.ExecuteTransaction(oCDO);
+
+                ResultStatus oResultStatus = oCDOService.CommitTransaction();
+
+                if (oResultStatus.IsSuccess) textBox_Log.AppendText(oResultStatus.Message + System.Environment.NewLine);
+                else throw new Exception(oResultStatus.ExceptionData.Description);
+            }
+            catch(Exception ex)
+            {
+                textBox_Log.AppendText(ex.Message + System.Environment.NewLine);
+            }
+        }
+
+        private void btn_FactoryDelete(object sender, EventArgs e)
+        {
+            try
+            {
+                FactoryMaintService oCDOService = new FactoryMaintService(new UserProfile(id, pw));
+                FactoryMaint oCDO = new FactoryMaint();
+
+                oCDO.ObjectToChange = new NamedObjectRef { Name = "Factory 1" };
+                oCDOService.BeginTransaction();
+                oCDOService.Delete(oCDO);
+                oCDOService.ExecuteTransaction();
+
+                ResultStatus oResultStatus = oCDOService.CommitTransaction();
+
+                if (oResultStatus.IsSuccess) textBox_Log.AppendText(oResultStatus.Message + System.Environment.NewLine);
+                else throw new Exception(oResultStatus.ExceptionData.Description);
+            }
+            catch (Exception ex)
+            {
+                textBox_Log.AppendText(ex.Message + System.Environment.NewLine);
+            }
+        }
+    }
+}
